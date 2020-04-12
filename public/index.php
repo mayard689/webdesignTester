a4c74e12dda85141a8404425ed3fa39c862fadc0
mayard689 <doc-albert@laposte.net>
2020-04-12 18:59:48 +0200

    color change for all elements h1 h2 h3 p body and sections

--- public/index.php
+++ public/index.php
@@ -24,9 +24,34 @@
                 <input id="__h1{color:" type="color" name="__h1{color:" value="<?= $_GET['__h1{color:'] ?? '#000000' ?>">
             </div>
 
+            <div class="styleSet">
+                <label>Couleur du titre 2</label>
+                <input type="color" name="__h2{color:" value="<?=$_GET['__h2{color:'] ?? '#000000' ?>">
+            </div>
+
+            <div class="styleSet">
+                <label>Couleur du titre 3</label>
+                <input type="color" name="__h3{color:" value="<?=$_GET['__h3{color:'] ?? '#000000' ?>">
+            </div>
+
+            <div class="styleSet">
+                <label>Couleur du paragraphe</label>
+                <input type="color" name="__p{color:" value="<?=$_GET['__p{color:'] ?? '#000000' ?>">
+            </div>
+
+            <label>Couleur du body</label>
+            <input type="color" name="___content{background-color:" value="<?=$_GET['___content{background-color:']??'#FFFFFF' ?>">
+            
+	    <label>Couleur de section 1</label>
+            <input type="color" name="___section1{background-color:" value="<?=$_GET['___section1{background-color:'] ?? '#FFFFFF' ?>">
+
+            <label>Couleur de section2</label>
+            <input type="color" name="___section2{background-color:" value="<?=$_GET['___section2{background-color:'] ?? '#FFFFFF' ?>">
+
             <input type="submit" value="tester">
 
         </form>
+
 
     <div class="content" >
 
