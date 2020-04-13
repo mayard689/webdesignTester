20381eccf4999405fad854797f29765a136405e3
mayard689 <doc-albert@laposte.net>
2020-04-13 11:19:54 +0200

    adding image

--- public/index.php
+++ public/index.php
@@ -112,11 +112,13 @@
                     <div class="myDiv">
                         <h3>Ceci est un titre de niveau 3</h3>
                         <p>Ceci est un paragraphe. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        <img class=myImage" src="/assets/pictures/peur.jpeg" alt="votre image ici" title="image de test">
                     </div>
 
                     <div class="myDiv">
                         <h3>Ceci est un titre de niveau 3</h3>
                         <p>Ceci est un paragraphe. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        <img class=myImage" src="/assets/pictures/peur.jpeg" alt="votre image ici" title="image de test">
                     </div>
                 </div>
             </div>
@@ -132,11 +134,13 @@
                     <div class="myDiv">
                         <h3>Ceci est un titre de niveau 3</h3>
                         <p>Ceci est un paragraphe. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        <img class=myImage" src="/assets/pictures/peur.jpeg" alt="votre image ici" title="image de test">
                     </div>
 
                     <div class="myDiv">
                         <h3>Ceci est un titre de niveau 3</h3>
                         <p>Ceci est un paragraphe. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        <img class=myImage" src="/assets/pictures/peur.jpeg" alt="votre image ici" title="image de test">
                     </div>
                 </div>
             </div>
